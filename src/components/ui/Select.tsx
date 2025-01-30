0c2fa98099b10f6c5729a038ec2dd0e6d63c7759
Maryammmy <malaksameh18112004@gmail.com>
2025-01-30 20:26:51 +0200

    change bg

--- src/components/ui/Select.tsx
+++ src/components/ui/Select.tsx
@@ -6,7 +6,10 @@ interface IProps extends SelectHTMLAttributes<HTMLSelectElement> {
 }
 function Select({ options, ...rest }: IProps) {
   return (
-    <select className="w-full border border-gray-300 rounded-lg p-2" {...rest}>
+    <select
+      className="w-full border bg-white border-gray-300 rounded-lg p-2"
+      {...rest}
+    >
       {options.map((option, index) => (
         <option key={index} value={option.value}>
           {option.label}
