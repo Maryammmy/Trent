9690fa14b761f97a9664f5589b874a2071654cb1
Maryammmy <malaksameh18112004@gmail.com>
2025-02-19 15:37:01 +0200

    update navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -7,17 +7,18 @@ import NavbarButtons from "./NavbarButtons";
 import useNavbarBg from "../../hooks/useNavbarBg";
 import Button from "../ui/Button";
 import { Menu } from "lucide-react";
+import { useAppSelector } from "../../store/hooks";
 
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
   const { pathname } = useLocation();
   const isSmallScreen = useMediaQuery({ maxWidth: 1023 });
+  const { bg } = useAppSelector((state) => state.navbar);
   useNavbarBg();
-
   return (
     <nav
       className={`top-0 left-0 w-full z-30 fixed ${
-        pathname === "/" ? "bg-transparent absolute" : "bg-primary"
+        pathname === "/" && !bg ? "bg-transparent absolute" : "bg-primary"
       }`}
     >
       <div className="max-w-[1450px] flex flex-wrap items-center justify-between mx-auto p-4">
