0c7928b6e304b00ef73ca6c81c5ecff6bdffe481
Maryammmy <malaksameh18112004@gmail.com>
2025-02-24 16:35:18 +0200

    filter services

--- src/services/filterService.ts
+++ src/services/filterService.ts
@@ -1,9 +1,9 @@
-import { useQuery } from "@tanstack/react-query";
-import { baseAPI } from ".";
+// import { useQuery } from "@tanstack/react-query";
+// import { baseAPI } from ".";
 
-export const useUserConversationAPI = () => {
-  return useQuery({
-    queryKey: ["useUserConversationAPI", id],
-    queryFn: () => baseAPI.get(`messaging/messages/user/${id}`),
-  });
-};
+// export const useUserConversationAPI = () => {
+//   return useQuery({
+//     queryKey: ["useUserConversationAPI", id],
+//     queryFn: () => baseAPI.get(`messaging/messages/user/${id}`),
+//   });
+// };
