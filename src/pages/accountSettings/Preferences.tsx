ccc3b37e2080fc3300a2c5bc64da1e68f67a8a59
Maryammmy <malaksameh18112004@gmail.com>
2025-03-03 11:27:58 +0200

    remove prefem=nce card

--- src/pages/accountSettings/Preferences.tsx
+++ src/pages/accountSettings/Preferences.tsx
@@ -1,7 +1,6 @@
 import PreferencesData from "../../components/accountSettings/preferences/PreferencesData";
 import DynamicTitle from "../../components/accountSettings/DynamicTitle";
 import { useTranslation } from "react-i18next";
-import PreferencesCard from "../../components/accountSettings/preferences/PreferencesCard";
 
 function Preferences() {
   const { t } = useTranslation();
@@ -10,7 +9,6 @@ function Preferences() {
       <DynamicTitle title={t("global_preferences")} />
       <div className="flex flex-col lg:flex-row lg:items-center gap-10 xl:gap-20 py-5 lg:py-10">
         <PreferencesData />
-        <PreferencesCard />
       </div>
     </div>
   );
