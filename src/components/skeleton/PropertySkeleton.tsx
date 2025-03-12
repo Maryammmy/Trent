06f0545fe66509a66f3dce5389ec1ad2e8f6287f
Maryammmy <malaksameh18112004@gmail.com>
2025-03-12 19:42:27 +0200

    property skeleton

--- src/components/skeleton/PropertySkeleton.tsx
+++ src/components/skeleton/PropertySkeleton.tsx
@@ -1,5 +1,25 @@
 function PropertySkeleton() {
-  return <div>PropertySkeleton</div>;
+  return (
+    <div>
+      <h3 className=" bg-stone-200 rounded-md h-8 w-1/4 animate-pulse mb-6"></h3>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
+        {Array.from({ length: 3 }).map((_, index) => (
+          <div
+            key={index}
+            className="w-full h-[250px] lg:h-[300px] bg-stone-200 animate-pulse rounded-md overflow-hidden"
+          ></div>
+        ))}
+      </div>
+      <div className="flex flex-col gap-5 pt-8">
+        {Array.from({ length: 3 }).map((_, index) => (
+          <div
+            key={index}
+            className="w-1/2 h-10 bg-stone-200 animate-pulse rounded-md overflow-hidden"
+          ></div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default PropertySkeleton;
--- src/pages/property/index.tsx
+++ src/pages/property/index.tsx
@@ -14,6 +14,7 @@ import {
 import { Grip } from "lucide-react";
 import { CurrentLanguage } from "../../types";
 import { baseURL } from "../../services";
+import PropertySkeleton from "../../components/skeleton/PropertySkeleton";
 
 const currentLanguage = localStorage.getItem("i18nextLng") as CurrentLanguage;
 function Property() {
@@ -26,82 +27,93 @@ function Property() {
   return (
     <>
       <div className="px-5 xl:px-20 py-5 lg:py-6">
-        <div className="pb-6">
-          <h2 className="font-bold text-2xl text-stone-800">
-            {propertyDetails?.title?.[currentLanguage]}
-          </h2>
-        </div>
-        <div className="relative">
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
-            {propertyDetails?.image_list?.slice(0, 3)?.map((image, i) => (
-              <div
-                key={i}
-                className={`w-full h-[250px] lg:h-[300px] rounded-md overflow-hidden`}
-              >
-                <Image
-                  className="w-full h-full object-cover"
-                  imageUrl={baseURL + image.img}
-                  alt={`Image ${i + 1}`}
-                />
-              </div>
-            ))}
-          </div>
-          <Link
-            to={`/properties/1/gallery`}
-            className="absolute bottom-2 right-8 flex items-center gap-1 bg-white py-1 px-2 border border-black rounded-md"
-          >
-            <span>
-              <Grip size={15} />
-            </span>
-            <span className="font-medium text-sm ">{t("show_all_photos")}</span>
-          </Link>
-        </div>
-        <div className="py-8 flex flex-col lg:flex-row gap-10 xl:gap-20 md:justify-between">
-          <div className="flex-[2]">
-            <div className="flex flex-col gap-1 pb-5">
-              <div className="font-medium text-black text-2xl flex gap-1">
-                <p> {propertyDetails?.city?.[currentLanguage]}</p>,
-                <p> {propertyDetails?.government?.name?.[currentLanguage]}</p>
-              </div>
-              <div className="font-medium flex gap-1">
-                <div className="flex gap-1">
-                  <p>{t("guest_count")}</p>
-                  <span>{propertyDetails?.guest_count}</span>
-                </div>
-                <span>,</span>
-                <div className="flex gap-1">
-                  <p>{t("beds_count")}</p>
-                  <span>{propertyDetails?.beds_count}</span>
-                </div>
-                <span>,</span>
-                <div className="flex gap-1">
-                  <p>{t("bathrooms_count")}</p>
-                  <span>{propertyDetails?.bathrooms_count}</span>
-                </div>
-              </div>
-              <div className="font-semibold">
-                <p>
-                  <span className="text-primary">
-                    {propertyDetails?.price} {t("price_per_night")}
-                  </span>{" "}
-                  <span className="text-dark">
-                    / {propertyDetails?.period?.name?.[currentLanguage]}
-                  </span>
-                </p>
-              </div>
+        {data ? (
+          <>
+            <div className="pb-6">
+              <h2 className="font-bold text-2xl text-stone-800">
+                {propertyDetails?.title?.[currentLanguage]}
+              </h2>
             </div>
-            <HostedBy
-              host={propertyDetails?.owner}
-              guestRules={propertyDetails?.guest_rules?.[currentLanguage]}
-            />
-            <Amenities facilities={facilities} />
-          </div>
-          <CheckDates />
-        </div>
-        <ReviewComponent />
-        <div className="max-w-7xl mx-auto py-5">
-          <Iframe width="100%" />
-        </div>
+            <div className="relative">
+              <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
+                {propertyDetails?.image_list?.slice(0, 3)?.map((image, i) => (
+                  <div
+                    key={i}
+                    className={`w-full h-[250px] lg:h-[300px] rounded-md overflow-hidden`}
+                  >
+                    <Image
+                      className="w-full h-full object-cover"
+                      imageUrl={baseURL + image.img}
+                      alt={`Image ${i + 1}`}
+                    />
+                  </div>
+                ))}
+              </div>
+              <Link
+                to={`/properties/1/gallery`}
+                className="absolute bottom-2 right-8 flex items-center gap-1 bg-white py-1 px-2 border border-black rounded-md"
+              >
+                <span>
+                  <Grip size={15} />
+                </span>
+                <span className="font-medium text-sm ">
+                  {t("show_all_photos")}
+                </span>
+              </Link>
+            </div>
+            <div className="py-8 flex flex-col lg:flex-row gap-10 xl:gap-20 md:justify-between">
+              <div className="flex-[2]">
+                <div className="flex flex-col gap-1 pb-5">
+                  <div className="font-medium text-black text-2xl flex gap-1">
+                    <p> {propertyDetails?.city?.[currentLanguage]}</p>,
+                    <p>
+                      {" "}
+                      {propertyDetails?.government?.name?.[currentLanguage]}
+                    </p>
+                  </div>
+                  <div className="font-medium flex gap-1">
+                    <div className="flex gap-1">
+                      <p>{t("guest_count")}</p>
+                      <span>{propertyDetails?.guest_count}</span>
+                    </div>
+                    <span>,</span>
+                    <div className="flex gap-1">
+                      <p>{t("beds_count")}</p>
+                      <span>{propertyDetails?.beds_count}</span>
+                    </div>
+                    <span>,</span>
+                    <div className="flex gap-1">
+                      <p>{t("bathrooms_count")}</p>
+                      <span>{propertyDetails?.bathrooms_count}</span>
+                    </div>
+                  </div>
+                  <div className="font-semibold">
+                    <p>
+                      <span className="text-primary">
+                        {propertyDetails?.price} {t("price_per_night")}
+                      </span>{" "}
+                      <span className="text-dark">
+                        / {propertyDetails?.period?.name?.[currentLanguage]}
+                      </span>
+                    </p>
+                  </div>
+                </div>
+                <HostedBy
+                  host={propertyDetails?.owner}
+                  guestRules={propertyDetails?.guest_rules?.[currentLanguage]}
+                />
+                <Amenities facilities={facilities} />
+              </div>
+              <CheckDates />
+            </div>
+            <ReviewComponent />
+            <div className="max-w-7xl mx-auto py-5">
+              <Iframe width="100%" />
+            </div>
+          </>
+        ) : (
+          <PropertySkeleton />
+        )}
       </div>
     </>
   );
