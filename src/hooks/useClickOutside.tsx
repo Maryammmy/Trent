e0c30dd76ea6ea16515adb540c0792e47141d513
Maryammmy <malaksameh18112004@gmail.com>
2025-04-07 17:48:59 +0200

    custom hook

--- src/hooks/useClickOutside.tsx
+++ src/hooks/useClickOutside.tsx
@@ -2,14 +2,16 @@ import { useEffect, RefObject } from "react";
 
 function useClickOutside(ref: RefObject<HTMLElement>, callback: () => void) {
   useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
+    const handleClickOutside = (event: MouseEvent | TouchEvent) => {
       if (ref.current && !ref.current.contains(event.target as Node)) {
         callback();
       }
     };
     document.addEventListener("mousedown", handleClickOutside);
+    document.addEventListener("touchstart", handleClickOutside);
     return () => {
       document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener("touchstart", handleClickOutside);
     };
   }, [ref, callback]);
 }
