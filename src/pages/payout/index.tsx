3f541191002cb5c38a78028abdc19e164e1b6ee5
Maryammmy <malaksameh18112004@gmail.com>
2025-04-16 22:03:11 +0200

    card

--- src/pages/payout/index.tsx
+++ src/pages/payout/index.tsx
@@ -1,5 +1,14 @@
+import { useTranslation } from "react-i18next";
+
 function Payout() {
-  return <div>Payout</div>;
+  const { t } = useTranslation();
+  return (
+    <div className="py-10">
+      <div className="mx-auto w-full md:w-[50%] lg:w-[40%] xl:w-[25%] bg-primary h-52 rounded-2xl">
+        <h2>{t("payout")}</h2>
+      </div>
+    </div>
+  );
 }
 
 export default Payout;
