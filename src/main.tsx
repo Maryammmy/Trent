c9a910e0858dae39450b93086675cd633610c3e8
Maryammmy <malaksameh18112004@gmail.com>
2025-04-19 16:19:32 +0200

    remove unnesserecy package

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { createRoot } from "react-dom/client";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "react-photo-view/dist/react-photo-view.css";
-import "rc-slider/assets/index.css";
 import "aos/dist/aos.css";
 import "./index.css";
 import App from "./App.tsx";
