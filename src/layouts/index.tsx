62057a03324e011db70076aa46e5aecfaae68583
Maryammmy <malaksameh18112004@gmail.com>
2025-05-10 13:25:10 +0300

    navbar

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,16 +1,9 @@
 import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../components/Footer";
 import Navbar from "../components/navbar/Navbar";
-import { useEffect } from "react";
-import { useAppDispatch } from "../store/hooks";
-import { setToggle } from "../store/features/navbar/navbarSlice";
 
 const RootLayout = () => {
   const { pathname } = useLocation();
-  const dispatch = useAppDispatch();
-  useEffect(() => {
-    dispatch(setToggle(false));
-  }, [pathname, dispatch]);
   return (
     <>
       <Navbar />
